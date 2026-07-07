e08ad1562d99bb66e1c6caa2c5daced1f85bd3eb
Miguelfc123 <your-email@example.com>
2026-07-07 00:34:13 -0300

    Decrease TheEyes card image size

--- Frontend-App/src/pages/PaginaInicial.js
+++ Frontend-App/src/pages/PaginaInicial.js
@@ -68,6 +68,7 @@ function PaginaInicial() {
             image: process.env.PUBLIC_URL + '/imagens/camisa/theEyesCarrosel.png',
             hoverImage: process.env.PUBLIC_URL + '/Cards/theeyes-hover.jpg',
             category: 'camiseta',
+            imageScale: 0.8,
           },
         ]);
       } catch (error) {
